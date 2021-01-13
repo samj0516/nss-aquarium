7dd29d55ed46b405f7cbdaaafdd046793ef50233
samj0516 <samj0516@gmail.com>
2021-01-13 09:29:18 -0600

    added fish data provider module

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,48 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Bart",
+        species: "dogfish shark",
+        length: "3-4 ft",
+        diet: "jelly fish",
+        location:"North Atlantic and Pacific"
+        
+    }
+    {
+        name: "Nemo",
+        species: "clownfish",
+        length: "5 in",
+        diet: "plankton",
+        location:"Great Barrier Reef, Australia"
+        
+    }
+    {
+        name: "Jack Dempsy",
+        species: "cichlid fish",
+        length: "10-15 in",
+        diet: "carnivorous",
+        location:"Southern Mexico"
+        
+    }
+    {
+        name: "Leo",
+        species: "lionfish",
+        length: "7-8 in",
+        diet: "algae",
+        location:"Indo-Pacific"
+        
+    }
+    {
+        name: "Polly",
+        species: "parrotfish",
+        length: "12-20 in",
+        diet: "polyps",
+        location:"Amazon Basin"
+        
+    }
+]
