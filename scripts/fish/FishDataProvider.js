001e1811e25acd36c75f1a1576cf1f9a4479174f
samj0516 <samj0516@gmail.com>
2021-01-13 13:03:30 -0600

    added scripts to automate fish cards

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -10,39 +10,46 @@ const fishCollection = [
         species: "dogfish shark",
         length: "3-4 ft",
         diet: "jelly fish",
-        location:"North Atlantic and Pacific"
+        location:"North Atlantic and Pacific",
+        image: "images/dogFish.jpeg"
         
-    }
+    },
     {
         name: "Nemo",
         species: "clownfish",
         length: "5 in",
         diet: "plankton",
-        location:"Great Barrier Reef, Australia"
-        
-    }
+        location:"Great Barrier Reef, Australia",
+        image: "images/clownfish.jpeg"
+    },
     {
         name: "Jack Dempsy",
         species: "cichlid fish",
         length: "10-15 in",
         diet: "carnivorous",
-        location:"Southern Mexico"
+        location:"Southern Mexico",
+        image: "images/jackdempsyFish.jpeg"
         
-    }
+    },
     {
         name: "Leo",
         species: "lionfish",
         length: "7-8 in",
         diet: "algae",
-        location:"Indo-Pacific"
+        location:"Indo-Pacific",
+        image: "images/lionFish.jpeg"
         
-    }
+    },
     {
         name: "Polly",
         species: "parrotfish",
         length: "12-20 in",
         diet: "polyps",
-        location:"Amazon Basin"
+        location:"Amazon Basin",
+        image: "images/parrotFish.jpeg"
         
     }
 ]
+export const useFish = () => {
+    return fishCollection.slice()
+}
---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import { useFish } from './fish/FishDataProvider.js'
+import { FishList } from './fish/FishList.js'
+
+// const allTheFish = useFish()
+
+// for (const fish of allTheFish) {
+//     console.log(fish)
+// }
+FishList()
